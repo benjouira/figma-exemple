9261c06153629d28f0d48e05e56ffb69625cd64f
Rabii <rabi3jouira@gmail.com>
2021-10-15 19:45:59 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediphone11promax1widget/GeneratedIPhone11ProMax1Widget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediphone11promax2widget/GeneratedIPhone11ProMax2Widget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedsearchwidget/GeneratedSearchWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconheartwidget/GeneratedIconheartWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconshoppingcartwidget/GeneratedIconshoppingcartWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconpluswidget/GeneratedIconplusWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconminuswidget/GeneratedIconminusWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconuserwidget/GeneratedIconuserWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconsearchwidget1/GeneratedIconsearchWidget1.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedchipsselecetdwidget/GeneratedChipsSelecetdWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedchipsunselecetdwidget/GeneratedChipsUnselecetdWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconcheckwidget1/GeneratedIconcheckWidget1.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedicongridwidget/GeneratedIcongridWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedtabbarwidget/GeneratedTabBarWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedtabwidget3/GeneratedTabWidget3.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedtabselectedwidget1/GeneratedTabSelectedWidget1.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconchevronleftwidget/GeneratedIconchevronleftWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generateditem_cardwidget/GeneratedItem_CardWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generateditem_rowviewwidget/GeneratedItem_RowViewWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconhomewidget/GeneratedIconhomeWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconxwidget/GeneratedIconxWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconbikewidget/GeneratedIconbikeWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconwalkingwidget/GeneratedIconwalkingWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedicondronewidget/GeneratedIconDroneWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedcell_rowviewwidget/GeneratedCell_RowviewWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatednavigationbarwidget/GeneratedNavigationBarWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconcreditcardwidget/GeneratedIconcreditcardWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatediconcamerawidget/GeneratedIconcameraWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedtextfieldwidget/GeneratedTextFieldWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedbuttonprimarywithiconwidget/GeneratedButtonPrimarywithIconWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedbuttonprimarywidget/GeneratedButtonPrimaryWidget.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedbuttonsecondarywidget1/GeneratedButtonSecondaryWidget1.dart';
+import 'package:flutterapp/figma_to_flutterapp/generatedbuttontextwidget/GeneratedButtonTextWidget.dart';
+
+void main() {
+  runApp(figma_to_flutterApp());
+}
+
+class figma_to_flutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedIPhone11ProMax1Widget',
+      routes: {
+        '/GeneratedIPhone11ProMax1Widget': (context) =>
+            GeneratedIPhone11ProMax1Widget(),
+        '/GeneratedIPhone11ProMax2Widget': (context) =>
+            GeneratedIPhone11ProMax2Widget(),
+        '/GeneratedSearchWidget': (context) => GeneratedSearchWidget(),
+        '/GeneratedIconheartWidget': (context) => GeneratedIconheartWidget(),
+        '/GeneratedIconshoppingcartWidget': (context) =>
+            GeneratedIconshoppingcartWidget(),
+        '/GeneratedIconplusWidget': (context) => GeneratedIconplusWidget(),
+        '/GeneratedIconminusWidget': (context) => GeneratedIconminusWidget(),
+        '/GeneratedIconuserWidget': (context) => GeneratedIconuserWidget(),
+        '/GeneratedIconsearchWidget1': (context) =>
+            GeneratedIconsearchWidget1(),
+        '/GeneratedChipsSelecetdWidget': (context) =>
+            GeneratedChipsSelecetdWidget(),
+        '/GeneratedChipsUnselecetdWidget': (context) =>
+            GeneratedChipsUnselecetdWidget(),
+        '/GeneratedIconcheckWidget1': (context) => GeneratedIconcheckWidget1(),
+        '/GeneratedIcongridWidget': (context) => GeneratedIcongridWidget(),
+        '/GeneratedTabBarWidget': (context) => GeneratedTabBarWidget(),
+        '/GeneratedTabWidget3': (context) => GeneratedTabWidget3(),
+        '/GeneratedTabSelectedWidget1': (context) =>
+            GeneratedTabSelectedWidget1(),
+        '/GeneratedIconchevronleftWidget': (context) =>
+            GeneratedIconchevronleftWidget(),
+        '/GeneratedItem_CardWidget': (context) => GeneratedItem_CardWidget(),
+        '/GeneratedItem_RowViewWidget': (context) =>
+            GeneratedItem_RowViewWidget(),
+        '/GeneratedIconhomeWidget': (context) => GeneratedIconhomeWidget(),
+        '/GeneratedIconxWidget': (context) => GeneratedIconxWidget(),
+        '/GeneratedIconbikeWidget': (context) => GeneratedIconbikeWidget(),
+        '/GeneratedIconwalkingWidget': (context) =>
+            GeneratedIconwalkingWidget(),
+        '/GeneratedIconDroneWidget': (context) => GeneratedIconDroneWidget(),
+        '/GeneratedCell_RowviewWidget': (context) =>
+            GeneratedCell_RowviewWidget(),
+        '/GeneratedNavigationBarWidget': (context) =>
+            GeneratedNavigationBarWidget(),
+        '/GeneratedIconcreditcardWidget': (context) =>
+            GeneratedIconcreditcardWidget(),
+        '/GeneratedIconcameraWidget': (context) => GeneratedIconcameraWidget(),
+        '/GeneratedTextFieldWidget': (context) => GeneratedTextFieldWidget(),
+        '/GeneratedButtonPrimarywithIconWidget': (context) =>
+            GeneratedButtonPrimarywithIconWidget(),
+        '/GeneratedButtonPrimaryWidget': (context) =>
+            GeneratedButtonPrimaryWidget(),
+        '/GeneratedButtonSecondaryWidget1': (context) =>
+            GeneratedButtonSecondaryWidget1(),
+        '/GeneratedButtonTextWidget': (context) => GeneratedButtonTextWidget(),
+      },
+    );
+  }
+}
